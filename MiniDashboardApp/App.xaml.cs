81a8b924d3de8162b7e72f8ef821d2ea2ed8da0e
Ali Ezzo <3li.ezzo@gmail.com>
2025-07-20 03:41:06 +0200

    Initial commit

---
+++ MiniDashboardApp/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MiniDashboardApp
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+    protected override void OnStartup(StartupEventArgs e)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+            base.OnStartup(e);
+        }
+    }
+    }
